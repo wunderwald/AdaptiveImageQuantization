547392738008c62f4e929aabec4c01d89720b3db
wunderwald <moritzwunderwald@yahoo.de>
2021-05-29 15:10:50 +0200

    added example ain code

--- main.py
+++ main.py
@@ -14,45 +14,45 @@ img = Image.open("./input.jpg")
 pixels = img.load()
 dims = img.size
 
-# for thresh in [ 690, 4200, 6900, 42000, 420000, 690000 ]:
-#     # no edges
-#     outNoEdge = Image.new(mode="RGB", size=dims)
-#     quantize(
-#             cell=[ 0, 0, dims[0], dims[1] ], 
-#             inputPixels=pixels, 
-#             outputImage=outNoEdge, 
-#             thresh=thresh, 
-#             showEdges=False, 
-#         )
-#     outNoEdge.save("./out_{thresh}_noEdge.jpg".format(thresh=thresh))
-#     outNoEdge.close()
-
-#     # with edges
-#     for edgeType in [ "inv", "black", "white" ]:
-#         out = Image.new(mode="RGB", size=dims)
-#         quantize(
-#             cell=[ 0, 0, dims[0], dims[1] ], 
-#             inputPixels=pixels, 
-#             outputImage=out, 
-#             thresh=thresh, 
-#             showEdges=True, 
-#             edgeType=edgeType
-#         )
-#         out.save("./out_{thresh}_{edge}.jpg".format(thresh=thresh, edge=edgeType))
-#         out.close()
-
-for thresh in [ 42000 ]:
+for thresh in [ 690, 4200, 6900, 42000, 420000, 690000 ]:
     # no edges
-    out = Image.new(mode="RGB", size=dims)
+    outNoEdge = Image.new(mode="RGB", size=dims)
     quantize(
+            cell=[ 0, 0, dims[0], dims[1] ], 
+            inputPixels=pixels, 
+            outputImage=outNoEdge, 
+            thresh=thresh, 
+            showEdges=False, 
+        )
+    outNoEdge.save("./out_{thresh}_noEdge.jpg".format(thresh=thresh))
+    outNoEdge.close()
+
+    # with edges
+    for edgeType in [ "inv", "black", "white" ]:
+        out = Image.new(mode="RGB", size=dims)
+        quantize(
             cell=[ 0, 0, dims[0], dims[1] ], 
             inputPixels=pixels, 
             outputImage=out, 
             thresh=thresh, 
-            showEdges=False
+            showEdges=True, 
+            edgeType=edgeType
         )
-    out.save("./out_{thresh}.jpg".format(thresh=thresh))
-    out.close()
+        out.save("./out_{thresh}_{edge}.jpg".format(thresh=thresh, edge=edgeType))
+        out.close()
+
+# for thresh in [ 42000 ]:
+#     # no edges
+#     out = Image.new(mode="RGB", size=dims)
+#     quantize(
+#             cell=[ 0, 0, dims[0], dims[1] ], 
+#             inputPixels=pixels, 
+#             outputImage=out, 
+#             thresh=thresh, 
+#             showEdges=False
+#         )
+#     out.save("./out_{thresh}.jpg".format(thresh=thresh))
+#     out.close()
 
 
 
