a491cde80d4bed7934c78017d202eb877532e45c
wunderwald <moritzwunderwald@yahoo.de>
2021-05-30 01:48:24 +0200

    added logging to parameterization demo

--- demo.py
+++ demo.py
@@ -15,6 +15,8 @@ def parameterization():
     for minCellDims in [ [4, 4], [6, 6], [8, 8] ]:
         for thresh in [ 6900, 42000, 420000, 690000 ]:
             for edgeType in [ "inv", "black", "white" ]:
+                print("{e} | {t} | {w}x{h}".format(e=edgeType, t=thresh, w=minCellDims[0], h=minCellDims[1]))
+
                 out = Image.new(mode="RGB", size=dims)
                 quantize(
                     cell=[ 0, 0, dims[0], dims[1] ], 
