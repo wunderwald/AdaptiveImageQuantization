b8bcbfaee594533c743af6e751f0c1e1fd37e1f4
wunderwald <moritzwunderwald@yahoo.de>
2021-05-30 01:46:53 +0200

    updated parameterization demo

--- demo.py
+++ demo.py
@@ -8,26 +8,26 @@ def parameterization():
     pixels = img.load()
     dims = img.size
 
-    minCellDims = [6, 6]
+    minCellDims = [8, 8]
 
     if(dims[0] != dims[1]):
         error("The code currently only works for sqaure images.")
-
-    for thresh in [ 690, 6900, 42000, 420000, 690000 ]:
-        for edgeType in [ "inv", "black", "white" ]:
-            out = Image.new(mode="RGB", size=dims)
-            quantize(
-                cell=[ 0, 0, dims[0], dims[1] ], 
-                inputPixels=pixels, 
-                outputImage=out, 
-                thresh=thresh, 
-                minCellDims=minCellDims,
-                showEdges=True, 
-                edgeType=edgeType
-            )
-            path = Path("./demoOut/out_{thresh}_{edge}_min{w}x{h}.png".format(thresh=thresh, edge=edgeType, w=minCellDims[0], h=minCellDims[1]))
-            out.save(path)
-            out.close()
+    for minCellDims in [ [4, 4], [6, 6], [8, 8] ]:
+        for thresh in [ 6900, 42000, 420000, 690000 ]:
+            for edgeType in [ "inv", "black", "white" ]:
+                out = Image.new(mode="RGB", size=dims)
+                quantize(
+                    cell=[ 0, 0, dims[0], dims[1] ], 
+                    inputPixels=pixels, 
+                    outputImage=out, 
+                    thresh=thresh, 
+                    minCellDims=minCellDims,
+                    showEdges=True, 
+                    edgeType=edgeType
+                )
+                path = Path("./demoOut/out_{thresh}_{edge}_min{w}x{h}.png".format(thresh=thresh, edge=edgeType, w=minCellDims[0], h=minCellDims[1]))
+                out.save(path)
+                out.close()
 
 def test():
     img = Image.open("./laikka.jpg")
